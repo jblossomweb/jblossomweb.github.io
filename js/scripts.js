cb0943f5e3098a4ca9801b5f195722a8e94df0f7
jblossom <jblossom@cinemaequip.com>
2014-06-10 09:29:13 -0400

    new layout, skin; pull navbar/quotes into templates

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
 $(function() {
 
+	template('templates/navbar.html', {brand: "John Blossom"}, function(template) {
+         $("#template-navbar").html(template);
+    });
 
 	template('templates/header.html', {name: "John Blossom",occupation:"Web Developer"}, function(template) {
          $("#template-header").html(template);
+    });
+
+    template('templates/carousel.html', {name: "John Blossom"}, function(template) {
+         $("#template-carousel").html(template).hide();
     });
 
 	$.get( jbzzle_api_url+"resume/testimonials", function( data ) {
@@ -11,10 +18,17 @@ $(function() {
 			        $(this).cycle({
 			        	slides: 	'> div',
 			        	fx: 		'fade',
-			        	speed: 		'1200', 
-			        });
+			        	speed: 		'5000', 
+			        }).hide();
 			 });
 	    });
 	}, "json" );
 
+});
+
+$( window ).load(function() {
+  // Run code
+  $("#template-carousel").fadeIn("500",function(){
+  		$("#template-quotes").show("250");
+  });
 });
