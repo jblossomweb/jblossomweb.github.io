98d5cdfaee55bc2fd80c5b0815a7bb8f4ce55461
jblossom <jblossom@cinemaequip.com>
2014-07-22 12:07:14 -0400

    bugfix prod

--- js/class/jbApp.js
+++ js/class/jbApp.js
@@ -51,8 +51,7 @@ jbApp.prototype.loadPage = function(page){
 			//http://ci-jbzzle.rhcloud.com/resume/skills
 			case 'skills':
 				$.get(app.apiURL+"resume/skills", function(data) {
-					console.log(data);
-					app.getTemplate('templates/skills.html', {skills:data}, function(template) {
+					app.getTemplate('templates/skills.html', {sets:data}, function(template) {
 						$("#template-skills").html(template);
 						//var element = $("#template-skills").find(".dots");
 						//app.initDots(element,4,150);
