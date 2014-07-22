37266ec2ed01fc1e065016fcacb320329c864910
jblossom <jblossom@cinemaequip.com>
2014-07-22 15:03:05 -0400

    new webservice host

--- js/api.js
+++ js/api.js
@@ -1,2 +1,3 @@
 //modify a local copy for dev
-var jbzzle_api_url = 'http://ci-jbzzle.rhcloud.com/';
+//var jbzzle_api_url = 'http://ci-jbzzle.rhcloud.com/';
+var jbzzle_api_url = 'http://jblossom.webfactional.com/';
