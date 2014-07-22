365e34112cacfca7e527712686b2685a3f5730e0
jblossom <jblossom@cinemaequip.com>
2014-07-22 12:40:41 -0400

    css change logo links

--- js/class/jbApp.js
+++ js/class/jbApp.js
@@ -51,7 +51,6 @@ jbApp.prototype.loadPage = function(page){
 			//http://ci-jbzzle.rhcloud.com/resume/skills
 			case 'skills':
 				$.get(app.apiURL+"resume/skills", function(data) {
-					console.log(data);
 					app.getTemplate('templates/skills.html', {sets:data}, function(template) {
 						$("#template-skills").html(template);
 						//var element = $("#template-skills").find(".dots");
