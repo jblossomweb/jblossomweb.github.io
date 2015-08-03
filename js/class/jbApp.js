f3951c89026904d072f22db36e7bd4e86c6ec7b3
John Blossom <jblossom.web@gmail.com>
2015-08-03 15:12:57 -0400

    refactor swipe again

--- js/class/jbApp.js
+++ js/class/jbApp.js
@@ -287,20 +287,6 @@ jbApp.prototype.loadPage = function(page){
 						$(".carousel").carousel({
 				      interval: 5000
 				    });
-				    
-						 //Enable swiping...
-						$(".carousel-inner").swipe( {
-							//Generic swipe handler for all directions
-							swipeLeft:function(event, direction, distance, duration, fingerCount) {
-								$(this).parent().carousel('prev'); 
-							},
-							swipeRight: function() {
-								$(this).parent().carousel('next'); 
-							},
-							//Default is 75px, set to 0 for demo so any distance triggers swipe
-							threshold:0
-						});
-
 
 						homeload.push('portfolio');
 						if(homeload.length > 1){
